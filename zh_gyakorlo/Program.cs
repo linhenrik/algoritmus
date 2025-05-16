988ee3cab99d191116342b9ef1b93c6319ae2b23
linhenrik <linhenrik@gmail.com>
2025-05-16 16:55:02 +0200

    commit

--- zh_gyakorlo/Program.cs
+++ zh_gyakorlo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace MyApp
 {
@@ -6,7 +7,28 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] tomb = new int[] { 13, 28, 43, 37, 10, 5, 8, 99, 101, 25 };
+            for (int i = 0; i < tomb.Length-1; i++)
+            {
+                int szamMax = i;
+                for (int j = i + 1; j < tomb.Length; j++)
+                {
+                    if (tomb[szamMax] < tomb[j])
+                    {
+                        szamMax = j;
+                    }
+                }
+                if (szamMax != i)
+                {
+                    int temp = tomb[i];
+                    tomb[i] = tomb[szamMax];
+                    tomb[szamMax] = temp;
+                }
+            }
+            System.Console.WriteLine("Rendezett tömb:");
+            foreach (int szam in tomb) {
+                System.Console.WriteLine(szam);
+            }
         }
     }
 }
